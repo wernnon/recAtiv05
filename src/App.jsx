c84d526661e627a1f0e4352c17cf350e4449a84f
wernnon <ruizwernnon@gmail.com>
2022-07-01 18:10:08 -0400

    codificação

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+//App.jsx
+import './App.css'
+import List from './List.jsx'
+
+function App(){
+
+return(
+ //bloco   
+    <div>
+        <div className='text'>
+            
+
+        </div>
+
+        <List/>  
+    
+    </div>
+)
+}
+export default App;
---
+++ src/List.jsx
@@ -0,0 +1,19 @@
+import './List.css'
+function List(){
+
+    return(
+        <div className='container'>
+            
+            <ol className="lista">
+              <li>Tools</li>
+              <li>Pricing</li>
+              <li>Blog</li>
+              <li>About</li>
+              <li>Customers</li>
+            </ol>
+            
+        </div>
+    );
+    
+}
+export default List;
